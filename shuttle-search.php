917ccbb8a5703bc71df0848e6764305c0f1ea860
Phil E. Taylor <phil@phil-taylor.com>
2021-11-28 20:51:23 +0000

    init - it works!

---
+++ shuttle-search.php
@@ -0,0 +1,74 @@
+<?php
+
+use Alfred\Workflows\Workflow;
+
+require 'vendor/autoload.php';
+
+$workflow = new Workflow;
+
+$AllknownHosts = file_get_contents('/Users/phil/.ssh/known_hosts');
+$knownHosts = explode("\n", $AllknownHosts);
+
+foreach ($knownHosts as $host) {
+    $hostParts = explode(' ', $host);
+
+
+    $matches = substr_count($hostParts[0], ':');
+
+    if ($matches > 1) {
+        continue;
+    }
+    $cmd = str_replace(':', ' -p ', $hostParts[0]);
+    $cmd = str_replace(['[', ']'], '', $cmd);
+
+    if ($cmd) {
+        $hosts[uniqid($cmd)] = [
+            'uniqid' => uniqid($cmd),
+            'subtitle' => 'ssh to ' . $cmd,
+            'cmd' => $cmd
+        ];
+    }
+}
+
+$json = json_decode(file_get_contents('/Users/phil/Dropbox/Apps/Shuttle/.shuttle.json'));
+foreach ($json->hosts as $group) {
+    foreach ($group as $entry) {
+        foreach ($entry as $one) {
+
+            $line = str_replace(['root@', 'ssh -t', 'ssh',], ['', '', ''], $one->cmd);
+            $line = trim($line);
+            if (substr_count($line, ':') > 1) {
+                continue;
+            }
+            if (substr_count($line, '-t') >= 1) {
+                continue;
+            }
+
+            $hosts[uniqid($line)] = [
+                'uniqid' => uniqid($one->cmd),
+                'subtitle' => $one->name,
+                'cmd' => $line
+            ];
+        }
+    }
+}
+
+ksort($hosts);
+
+foreach ($hosts as $host) {
+
+    $urlParts = explode(' ', $host['cmd']);
+    $icon = 'icon.png';
+    $workflow->result()
+        ->uid($host['uniqid'])
+        ->title($host['subtitle'] ?: $host['cmd'])
+        ->subtitle($host['subtitle'])
+        ->arg($host['cmd'])
+        ->quicklookurl('https://' . \count($urlParts) ? $urlParts[0] : $host)
+        ->icon($icon)
+        ->type('default')
+        ->valid(true)
+        ->autocomplete($host);
+}
+
+echo $workflow->output();
---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => '1.0.0+no-version-set',
+        'version' => '1.0.0.0',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => NULL,
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => '1.0.0+no-version-set',
+            'version' => '1.0.0.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => NULL,
+            'dev_requirement' => false,
+        ),
+        'joetannenbaum/alfred-workflow' => array(
+            'pretty_version' => '0.1.2',
+            'version' => '0.1.2.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../joetannenbaum/alfred-workflow',
+            'aliases' => array(),
+            'reference' => '88e042db9cad0c95ab80e87c69d7f1e15340e7ac',
+            'dev_requirement' => false,
+        ),
+    ),
+);
